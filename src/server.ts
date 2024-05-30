1c21d351020545645bee30d2ceb661d85630b157
Matheus Viscki <matheusviscki@gmail.com>
2024-05-29 22:54:58 -0300

    feat: install eslint. - configure ESLint following the external library "@Rocketseat"

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import {fastify} from "fastify";
+import { fastify } from 'fastify'
 
-const server = fastify();
+const server = fastify()
 
-server.get("/", async (request, reply) => {
-  return {hello: "world"};
-});
-
-server.listen({
-  port: 3000,
-}).then(() => {
-  console.log("Server is running on http://localhost:3000");
+server.get('/', async (request, reply) => {
+  return { hello: 'world' }
 })
+
+server
+  .listen({
+    port: 3000,
+  })
+  .then(() => {
+    console.log('Server is running on http://localhost:3000')
+  })
