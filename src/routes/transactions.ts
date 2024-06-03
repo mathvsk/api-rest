994b4aba1b995eb5954e629739d2755c414d06c8
Matheus Viscki <matheusviscki@gmail.com>
2024-06-03 17:51:19 -0300

    feat: Create routes to retrieve transactions. - single and all.

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -22,4 +22,22 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
     return reply.status(201).send()
   })
+
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionsParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where({ id }).first()
+
+    return { transaction }
+  })
 }
