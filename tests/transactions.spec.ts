c0cec2d1d5a4ba8b4865c7d5413bf0775bf4dc29
Matheus Viscki <matheusviscki@gmail.com>
2024-06-04 07:41:11 -0300

    refactor: Add test categorization. - Change function "test" to "it". To get semantically better

--- tests/transactions.spec.ts
+++ tests/transactions.spec.ts
@@ -1,19 +1,21 @@
-import { afterAll, beforeAll, expect, test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import { server } from '../src/app'
 import request from 'supertest'
 
-beforeAll(async () => {
-  await server.ready()
-})
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await server.ready()
+  })
 
-afterAll(async () => {
-  await server.close()
-})
+  afterAll(async () => {
+    await server.close()
+  })
 
-test('user can create a new transaction', async () => {
-  const response = await request(server.server)
-    .post('/transactions')
-    .send({ title: 'New transaction', amount: 5000, type: 'credit' })
+  it('should be able to create a new transaction', async () => {
+    const response = await request(server.server)
+      .post('/transactions')
+      .send({ title: 'New transaction', amount: 5000, type: 'credit' })
 
-  expect(response.status).toBe(201)
+    expect(response.status).toBe(201)
+  })
 })
