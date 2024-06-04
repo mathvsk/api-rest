a0ff4ac97e295a030d959c2e7698d37ef3ad81ac
Matheus Viscki <matheusviscki@gmail.com>
2024-06-04 07:55:58 -0300

    refactor: Create test to test route that retrieves all transactions.

--- tests/transactions.spec.ts
+++ tests/transactions.spec.ts
@@ -18,4 +18,24 @@ describe('Transactions routes', () => {
 
     expect(response.status).toBe(201)
   })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(server.server)
+      .post('/transactions')
+      .send({ title: 'New transaction', amount: 5000, type: 'credit' })
+
+    const cookies = createTransactionResponse.get('Set-Cookie') || []
+
+    const listTransactionsResponse = await request(server.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    ])
+  })
 })
