8c96ba977ad24ced56093f79f140531cd6f49255
Matheus Viscki <matheusviscki@gmail.com>
2024-06-04 16:21:42 -0300

    feat: Add necessary configurations to deploy the application.

--- src/databse.ts
+++ src/databse.ts
@@ -1,16 +1,24 @@
-import { knex as SetupKnex, Knex } from "knex";
+import { knex as SetupKnex, Knex } from 'knex'
 import { env } from './env'
 
-export const config: Knex.Config = {
-    client: "sqlite",
-    connection: {
-        filename: env.DATABASE_URL,
-    },
-    useNullAsDefault: true,
-    migrations: {
-        extension: "ts",
-        directory: "./db/migrations",
-    },
-};
+function getConnection(): string | Knex.StaticConnectionConfig {
+  if (env.DATABASE_CLIENT === 'sqlite') {
+    return {
+      filename: env.DATABASE_URL,
+    }
+  } else {
+    return env.DATABASE_URL
+  }
+}
 
-export const knex = SetupKnex(config);
+export const config: Knex.Config = {
+  client: env.DATABASE_CLIENT,
+  connection: getConnection(),
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+
+export const knex = SetupKnex(config)
--- src/env/index.ts
+++ src/env/index.ts
@@ -16,8 +16,9 @@ if (process.env.NODE_ENV === NodeEnv.Test) {
 
 const envSchema = z.object({
   NODE_ENV: z.nativeEnum(NodeEnv).default(NodeEnv.Production),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']).default('sqlite'),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3000),
+  PORT: z.coerce.number().default(3000),
 })
 
 const _env = envSchema.safeParse(process.env)
