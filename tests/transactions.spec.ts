57d0729aaeabe9e9a9b0614523b8fd4b86f704d1
Matheus Viscki <matheusviscki@gmail.com>
2024-06-04 08:32:36 -0300

    refactor: Create last tests; - get a specific transaction; - get balance

--- tests/transactions.spec.ts
+++ tests/transactions.spec.ts
@@ -44,4 +44,65 @@ describe('Transactions routes', () => {
       }),
     ])
   })
+
+  it('should be able to get a specific transaction', async () => {
+    const createTransactionResponse = await request(server.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie') || []
+
+    const listTransactionsResponse = await request(server.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    const transactionId = listTransactionsResponse.body.transactions[0].id
+
+    const getTransactionResponse = await request(server.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(getTransactionResponse.body.transaction).toEqual(
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      }),
+    )
+  })
+
+  it('should be able to get the balance', async () => {
+    const createTransactionResponse = await request(server.server)
+      .post('/transactions')
+      .send({
+        title: 'Credit transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie') || []
+
+    await request(server.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'Debit transaction',
+        amount: 2000,
+        type: 'debit',
+      })
+
+    const balanceResponse = await request(server.server)
+      .get('/transactions/balance')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(balanceResponse.body.balance).toEqual({
+      balance: 3000,
+    })
+  })
 })
