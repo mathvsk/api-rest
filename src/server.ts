d54531ec8845463d572e7ec46494c992e251e3a0
Matheus Viscki <matheusviscki@gmail.com>
2024-06-04 16:40:51 -0300

    feat: Add necessary configuration to deploy correctly on Render.

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { server } from './app'
 server
   .listen({
     port: env.PORT,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
     console.log(`Server is running on http://localhost:${env.PORT}`)
